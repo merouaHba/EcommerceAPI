017d37fbf0cabe46900a927c3bed70846d7db4aa
Meroua Hadj Benaichouche <marouam312@gmail.com>
2025-01-11 18:33:46 +0100

    enhance token

--- utils/createTokenUser.js
+++ utils/createTokenUser.js
@@ -1,6 +1,9 @@
 const createTokenUser = (user) => {
-  // console.log({ name: `${user.firstname} ${user.lastname}`, email: user.email, role: user.role })
-  return { _id:user._id, name: `${user.firstname} ${user.lastname}`, email: user.email, role: user.role , profilePicture: user.profilePicture?.url, mobile: user.mobile };
+  if (user.role === "seller") {
+    return { _id: user._id, name: `${user.firstname} ${user.lastname}`, firstname: user.firstname, lastname: user.lastname, email: user.email, role: user.role , profilePicture: user.profilePicture?.url, mobile: user.mobile ,address: user.address, storeName: user.storeName, storeDetails: user.storeDetails };
+    
+  }
+  return { _id:user._id, name: `${user.firstname} ${user.lastname}`,firstname: user.firstname, lastname: user.lastname, email: user.email, role: user.role , profilePicture: user.profilePicture?.url, mobile: user.mobile ,address: user.address, };
 };
 
 module.exports = createTokenUser;
