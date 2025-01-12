02aeb0db1dc2722d2c652a075c876f72f67053af
Meroua Hadj Benaichouche <marouam312@gmail.com>
2025-01-12 16:04:31 +0100

    enhance error handling

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -7,24 +7,38 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     return res.status(err.statusCode).json({ msg: err.message })
   }
 
-  // // Duplicate key error
-  // if (err.code === 11000) {
-  //   const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
-  //   return res.status(StatusCodes.BAD_REQUEST).json({ msg: message })
-  // }
+  // Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: message })
+  } 
 
-  // // wrong jwt error
-  // if (err.name === "JsonWebTokenError") {
-  //   const message = `Your url is invalid please try again letter`;
-  //   return res.status(StatusCodes.BAD_REQUEST).json({ msg: message })
+  // Validation  error
+  if (err.name === "ValidationError") {
+    const message = `${Object.values(err.errors).map((item) => item.message).join(', ')}`;
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: message })
+  }
 
-  // }
+  // Cast  error
+  if (err.name === "CastError") {
+    const message = `Invalid ${err.path} : ${err.value}`;
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: message })
+  }
 
-  // // jwt expired
-  // if (err.name === "TokenExpiredError") {
-  //   const message = `Your Url is expired please try again letter!`;
-  //   return res.status(StatusCodes.BAD_REQUEST).json({ msg: message })
-  // }
+  // wrong jwt error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Your url is invalid please try again letter`;
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: message })
+
+  }
+
+  // jwt expired
+  if (err.name === "TokenExpiredError") {
+    const message = `Your Url is expired please try again letter!`;
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: message })
+  }
+
+
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg:err.message })
 }
 
