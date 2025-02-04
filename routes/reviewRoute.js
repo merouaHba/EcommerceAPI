9c2e507daf9775d8772c7f23c2e7e6f28e340f13
Meroua Hadj Benaichouche <marouam312@gmail.com>
2025-02-04 16:11:08 +0100

    enhance review routes

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,14 +1,25 @@
-const express = require('express')
-const { createReview, getReview, updateReview, deleteReview, getReviewsByProduct } = require('../controllers/reviewController')
-const router = express.Router()
-const { authenticateUser, authorizePermissions } = require('../middlewares/authentication')
+const express = require('express');
+const router = express.Router();
+const {
+    createReview,
+    updateReview,
+    deleteReview,
+    getReviewsByProduct,
+    getReviewStats,
+    getUserReviews,
+    getMyReviews
+} = require('../controllers/reviewController');
+const {
+    authenticateUser,
+    authorizePermissions
+} = require('../middlewares/authentication');
 
+router.get('/stats/:productId', getReviewStats);
+router.get('/my-reviews', authenticateUser, authorizePermissions('user'), getMyReviews);
+router.get('/user/:userId?', authenticateUser, authorizePermissions('seller','admin'), getUserReviews);
+router.get('/:productId', getReviewsByProduct);
+router.post('/', authenticateUser, authorizePermissions('user'), createReview);
+router.put('/:id', authenticateUser, authorizePermissions('user'), updateReview);
+router.delete('/:id', authenticateUser, authorizePermissions('user'), deleteReview);
 
-router.get('/:productId', authenticateUser, getReviewsByProduct)
-router.post('/', authenticateUser, authorizePermissions('user'), createReview)
-router.get('/:id', authenticateUser, getReview)
-router.put('/:id', authenticateUser, authorizePermissions('user'), updateReview)
-router.delete('/:id', authenticateUser, authorizePermissions('user'), deleteReview)
-
-
-module.exports = router
+module.exports = router;
