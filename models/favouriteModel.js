1e4b25fb3e9a426a1d938cde599020f8d384a7ce
Meroua Hadj Benaichouche <marouam312@gmail.com>
2025-02-06 07:09:03 +0100

    enhance favourite model

--- models/favouriteModel.js
+++ models/favouriteModel.js
@@ -1,25 +1,30 @@
-
-
-const mongoose = require('mongoose')
-
-
+// favouriteModel.js
+const mongoose = require('mongoose');
 
 const FavouriteSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true,
+        required: [true, 'User ID is required'],
+        index: true // Add index for better query performance
     },
-    products: [
-        {
-                type: mongoose.Types.ObjectId,
-                ref: 'Product',
-           
-        }
-    ],
-})
+    products: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    }],
+}, {
+    timestamps: true // Add timestamps for better tracking
+});
 
+// Add compound index for faster queries
+FavouriteSchema.index({ user: 1, 'products': 1 });
 
-module.exports = mongoose.model('Favourite', FavouriteSchema)
+// Add method to check if product exists in favorites
+FavouriteSchema.methods.hasProduct = function (productId) {
+    return this.products.some(product =>
+        product.toString() === productId.toString()
+    );
+};
 
-
+module.exports = mongoose.model('Favourite', FavouriteSchema);
